4fbc156ea518c0773a58045f7690f5065d62276f
Nick Bohnsack <nick.bohnsack@gmail.com>
2022-08-05 21:07:17 +0200

    Imported a* pathfinding project

--- DeathIsTemporaryJam/Assets/Scripts/Player/Shooting.cs
+++ DeathIsTemporaryJam/Assets/Scripts/Player/Shooting.cs
@@ -10,27 +10,46 @@ public class Shooting : MonoBehaviour
 
     [SerializeField] float bulletSpeed;
     [SerializeField] float shootCooldown;
+    [SerializeField] int amountOfBullets;
 
     private Vector2 direction;
 
     private Rigidbody2D bulletRB;
     private GameObject bulletObject;
 
+    private float timeStamp = 0;
+
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+/*        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Fire();
+        }*/
+
+        if (Input.GetKey(KeyCode.Space))
         {
             Shoot();
+
         }
     }
 
-
+    
     void Shoot()
+    {
+        if (timeStamp <= Time.time)
+        {
+            Fire();
+            timeStamp = Time.time + shootCooldown;
+        }
+    }
+
+    void Fire()
     {
         direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
         bulletObject = Instantiate(bullet, shootingPoint.position, transform.rotation, bulletParent);
         bulletRB = bulletObject.GetComponent<Rigidbody2D>();
         bulletRB.AddForce(direction * bulletSpeed);
+
     }
 }
