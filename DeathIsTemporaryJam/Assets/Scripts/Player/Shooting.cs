75d8636957595de68c34a6e7757803a4417affa2
Nick Bohnsack <nick.bohnsack@gmail.com>
2022-08-05 18:00:59 +0200

    Implemented Basic Shooting

--- DeathIsTemporaryJam/Assets/Scripts/Player/Shooting.cs
+++ DeathIsTemporaryJam/Assets/Scripts/Player/Shooting.cs
@@ -5,25 +5,32 @@ using UnityEngine;
 public class Shooting : MonoBehaviour
 {
     [SerializeField] GameObject bullet;
-    [SerializeField] GameObject bulletParent;
+    [SerializeField] Transform bulletParent;
+    [SerializeField] Transform shootingPoint;
 
     [SerializeField] float bulletSpeed;
     [SerializeField] float shootCooldown;
 
     private Vector2 direction;
 
+    private Rigidbody2D bulletRB;
+    private GameObject bulletObject;
 
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Shoot();
+        }
     }
 
 
     void Shoot()
     {
         direction = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
-/*        Instantiate(bullet, );*/
-
+        bulletObject = Instantiate(bullet, shootingPoint.position, transform.rotation, bulletParent);
+        bulletRB = bulletObject.GetComponent<Rigidbody2D>();
+        bulletRB.AddForce(direction * bulletSpeed);
     }
 }
