f21da5312d18987f5940362d46a2a6c8d2f16df0
Petert_Gamerrr <497155@student.saxion.nl>
2022-08-05 18:01:33 +0200

    stat manager

---
+++ DeathIsTemporaryJam/Assets/Scripts/Managers/StatManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class StatManager : MonoBehaviour
+    {
+        private static StatManager _instance;
+        public static StatManager Instance
+        {
+            get
+            {
+                if (_instance == null) Debug.LogError("Gamemanager is null");
+                return _instance;
+            }
+        }
+
+        public int Speed = 0;
+        public int Health = 0;
+        public int GunRpm = 0;
+        public int BulletCount = 0;
+        public int Piercing = 0;
+        public int Explosion = 0;
+
+        private void Awake()
+        {
+            _instance = this;
+        }
+
+        public void Upgrade(Upgrades upgrade)
+        {
+            switch (upgrade)
+            {
+                case Upgrades.SPEED:
+                    Speed++;
+                    break;
+                case Upgrades.HEALTH:
+                    Health++;
+                    break;
+                case Upgrades.GUNRPM:
+                    GunRpm++;
+                    break;
+                case Upgrades.BULLETCOUNT:
+                    BulletCount++;
+                    break;
+                case Upgrades.PIERCING:
+                    Piercing++;
+                    break;
+                case Upgrades.EXPLOSION:
+                    Explosion++;
+                    break;
+            }
+        }
+        
+        public enum Upgrades
+        {
+            SPEED,
+            HEALTH,
+            GUNRPM,
+            BULLETCOUNT,
+            PIERCING,
+            EXPLOSION
+        }
+
+    }
+}
