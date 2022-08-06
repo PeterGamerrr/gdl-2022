662c43c07d2f9add04b8a068e8a1daabe0fcf724
Nick Bohnsack <nick.bohnsack@gmail.com>
2022-08-06 12:23:26 +0200

    Wave Spawn Incremental

--- DeathIsTemporaryJam/Assets/Scripts/Enemy/WaveSpawner.cs
+++ DeathIsTemporaryJam/Assets/Scripts/Enemy/WaveSpawner.cs
@@ -11,17 +11,34 @@ public class WaveSpawner : MonoBehaviour
 
     [SerializeField] float spawnBuffer;
 
+    [SerializeField] int[] enemyLevelWaves;
+    [SerializeField] int currentEnemyLevel;
+    [SerializeField] int startEnemyAmount;
+    [SerializeField] float waveMultiplier;
+    [SerializeField] int waveCooldownInSeconds;
+
+
+
     Camera cam;
 
     private Vector3 spawnPosition;
     private float spawnX;
     private float spawnY;
     private float outerSpawn;
-
     private int axisRandomiser;
+
+
+    private int currentWave = 0;
+    private int currentAmountOfEnemies;
+    private float currentWaveMultiplier;
+    private List<GameObject> waveEnemies = new List<GameObject>();
+    private bool waveEnded = true;
+
+
     void Start()
     {
         cam = Camera.main;
+        currentAmountOfEnemies = startEnemyAmount;
     }
 
 
@@ -29,14 +46,18 @@ public class WaveSpawner : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Return))
         {
-            GetSpawnPositions();
+            StartCoroutine(SpawnNextWave());
         }
+
+        UpdateWaves();
+
+
     }
 
 
-    void GetSpawnPositions()
+    Vector3 GetSpawnPosition()
     {
-        outerSpawn = Random.Range(-1, 1);
+        outerSpawn = Random.Range(-1f, 1f);
         if (outerSpawn >= 0)
         {
             outerSpawn += 1.1f;
@@ -46,18 +67,70 @@ public class WaveSpawner : MonoBehaviour
         if (axisRandomiser == 0)
         {
             spawnX = outerSpawn;
-            spawnY = Random.Range(-1, 3);
+            spawnY = Random.Range(-1f, 3f);
         }
         else
         {
             spawnY = outerSpawn;
-            spawnX = Random.Range(-1, 3);
+            spawnX = Random.Range(-1f, 3f);
         }
 
         spawnPosition = cam.ViewportToWorldPoint(new Vector3(spawnX, spawnY, 1));
 
+        //TODO: check if position is safe
 
-        Instantiate(enemyTypes[0], spawnPosition, transform.rotation, enemyParent);
+        return spawnPosition;
+    }
+
+
+    void IncreaseWave()
+    {
+        currentWave++;
+
+        //give score?
+    }
+
+
+    void SpawnEnemies(int amountOfMobs, int enemyLVL)
+    {
+        int randomMobID;
+        Vector3 spawnPos;
+        for (int i = 0; i < amountOfMobs; i++)
+        {
+            randomMobID = Random.Range(0, enemyLVL);
+            spawnPos = GetSpawnPosition();
+            waveEnemies.Add(Instantiate(enemyTypes[randomMobID], spawnPos, transform.rotation, enemyParent));
+        }
+    }
+
+    void SpawnWaves(int wave)
+    {
+        Debug.Log(currentWaveMultiplier);
+        currentAmountOfEnemies = (int) (currentAmountOfEnemies  * waveMultiplier);
+
+        SpawnEnemies(currentAmountOfEnemies, currentEnemyLevel);
+    }
+
+    IEnumerator SpawnNextWave()
+    {
+
+        IncreaseWave();
+        Debug.Log("Spawning wave " + currentWave + " in " + waveCooldownInSeconds + " seconds.");
+        yield return new WaitForSeconds(waveCooldownInSeconds);
+        SpawnWaves(currentWave);
+        Debug.Log("Spawned wave " + currentWave);
+    }
+
+    void UpdateWaves()
+    {
+
+
+        if (waveEnded)
+        {
+            waveEnded = false;
+            StartCoroutine(SpawnNextWave());
+        }
+
     }
 
 }
