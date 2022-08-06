c95d4f93b06df5f0158456063a394aec72673728
Petert_Gamerrr <497155@student.saxion.nl>
2022-08-06 22:13:25 +0200

    gamemanager

--- DeathIsTemporaryJam/Assets/Scripts/Managers/GameManager.cs
+++ DeathIsTemporaryJam/Assets/Scripts/Managers/GameManager.cs
@@ -1,18 +1,70 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    private static GameManager _instance;
+    public static GameManager Instance
+    {
+        get
+        {
+            if (_instance == null) Debug.LogError("Gamemanager is null");
+            return _instance;
+        }
+    }
+
+    private SceneManager _sceneManager = new();
+
+    [SerializeField] HealthController HealthController;
+    [SerializeField] LivesController LivesController;
+
+    [SerializeField] GameObject HealthBar;
+    [SerializeField] GameObject StartMenu;
+    [SerializeField] GameObject GameOverMenu;
+    [SerializeField] GameObject UpgradeMenu;
+
+
     void Start()
     {
+        HealthController.DeathEvent.AddListener(OnDeath);
+        LivesController.GameOverEvent.AddListener(OnGameOver);
+    }
+
+    public void StartGame()
+    {
+        StartMenu.SetActive(false);
+        HealthBar.SetActive(true);
+    }
+
+    public void OnDeath()
+    {
+        UpgradeMenu.SetActive(true);
+        HealthBar.SetActive(false);
         
     }
 
-    // Update is called once per frame
-    void Update()
+    public void ContinueAfterDeath()
     {
-        
+        UpgradeMenu.SetActive(false);
+        HealthBar.SetActive(true);
+
+    }
+
+    public void OnGameOver()
+    {
+        UpgradeMenu.SetActive(false);
+        GameOverMenu.SetActive(true);
+        HealthBar.SetActive(false);
+
+    }
+
+    public void ContinueAfterGameOver()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+
     }
 }
--- DeathIsTemporaryJam/Assets/Scripts/Managers/StatManager.cs
+++ DeathIsTemporaryJam/Assets/Scripts/Managers/StatManager.cs
@@ -2,7 +2,8 @@
 using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
-
+using UnityEngine.UI;
+using TMPro;
 namespace Assets.Scripts
 {
     public class StatManager : MonoBehaviour
@@ -29,6 +30,15 @@ namespace Assets.Scripts
         public int BulletCount = 0;
         public int Piercing = 0;
         public int Explosion = 0;
+
+        public TextMeshProUGUI SpeedVisual;
+        public TextMeshProUGUI HealthVisual;
+        public TextMeshProUGUI GunVisual;
+        public TextMeshProUGUI DamageVisual;
+        public TextMeshProUGUI BulletCountVisual;
+        public TextMeshProUGUI PiercingVisual;
+        public TextMeshProUGUI ExplosionVisual;
+        public TextMeshProUGUI UpgradesVisual;
 
         private void Awake()
         {
@@ -57,6 +67,9 @@ namespace Assets.Scripts
                     break;
                 case Upgrades.EXPLOSION:
                     Explosion++;
+                    break;
+                case Upgrades.DAMAGE:
+                    Damage++;
                     break;
             }
             UpgradePoints--;
@@ -112,6 +125,18 @@ namespace Assets.Scripts
         public void GiveUpgradePoint()
         {
             UpgradePoints++;
+        }
+
+        private void Update()
+        {
+            BulletCountVisual.text = "5" + BulletCount;
+            DamageVisual.text = "5" + Damage;
+            ExplosionVisual.text = "5" + Explosion;
+            GunVisual.text = "5" + GunRpm;
+            HealthVisual.text = "5" + Health;
+            PiercingVisual.text = "5" + Piercing;
+            SpeedVisual.text = "5" + Speed;
+            UpgradesVisual.text = "5" + UpgradePoints;
         }
     }
 }
