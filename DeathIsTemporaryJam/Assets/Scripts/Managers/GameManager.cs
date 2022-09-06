e75817dc0cb040fc58de1e8d4cd1f02a5273699b
PeterGamerrr <peterrr@assenbergh.nl>
2022-09-06 22:46:19 +0200

    kade toggle pause only be able to be executed every n seconds

--- DeathIsTemporaryJam/Assets/Scripts/Managers/GameManager.cs
+++ DeathIsTemporaryJam/Assets/Scripts/Managers/GameManager.cs
@@ -2,6 +2,7 @@ using Assets.Scripts;
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.InputSystem;
 using UnityEngine.SceneManagement;
@@ -18,24 +19,26 @@ public class GameManager : MonoBehaviour
             return _instance;
         }
     }
+    
     public GameState GameState = GameState.MAINMENU;
-
-    private SceneManager _sceneManager = new();
+    public List<GameObject> Bullets = new List<GameObject>();
 
     [SerializeField] HealthController HealthController;
     [SerializeField] LivesController LivesController;
     [SerializeField] WaveSpawner WaveSpawner;
 
+    [Header("UI")]
     [SerializeField] GameObject HealthBar;
     [SerializeField] GameObject StartMenu;
     [SerializeField] GameObject GameOverMenu;
     [SerializeField] GameObject UpgradeMenu;
     [SerializeField] GameObject GameNumbers;
     [SerializeField] GameObject PauseMenu;
+    [SerializeField] GameObject SettingsMenu;
 
-
-    public List<GameObject> Bullets = new List<GameObject>();
-
+    private DateTime _pauseCooldownTimeStamp;
+    private const float _pauseCooldownTime = 1;
+    
     private void Awake()
     {
         _instance = this;
@@ -50,12 +53,14 @@ public class GameManager : MonoBehaviour
 
     public void TogglePause(InputAction.CallbackContext context)
     {
-        Debug.LogError("CODE IS ALIVE");
+        if (_pauseCooldownTimeStamp >= DateTime.Now) return;
+        _pauseCooldownTimeStamp = DateTime.Now.AddSeconds(_pauseCooldownTime);
+        
         if (GameState == GameState.PLAYING)
         {
             Pause();
         }
-        if (GameState == GameState.PAUSE)
+        else if (GameState == GameState.PAUSE)
         {
             UnPause();
         }
@@ -66,6 +71,7 @@ public class GameManager : MonoBehaviour
         HealthBar.SetActive(true);
         GameNumbers.SetActive(true);
         Time.timeScale = 1;
+        GameState = GameState.PLAYING;
     }
 
     public void Pause()
@@ -74,6 +80,7 @@ public class GameManager : MonoBehaviour
         HealthBar.SetActive(false);
         GameNumbers.SetActive(false);
         Time.timeScale = 0;
+        GameState = GameState.PAUSE;
     }
 
     public void StartGame()
@@ -124,6 +131,15 @@ public class GameManager : MonoBehaviour
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 
+    public void ShowSettings()
+    {
+        SettingsMenu.SetActive(false);
+    }
+
+    public void HideSettings()
+    {
+        SettingsMenu.SetActive(false);
+    }
 
     private void RemoveBullets()
     {
