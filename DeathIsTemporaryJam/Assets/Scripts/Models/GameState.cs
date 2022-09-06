c1351b0c1a10e6a4aeff5e37f1d8615e38a2a627
PeterGamerrr <peterrr@assenbergh.nl>
2022-09-06 12:02:53 +0200

    pause menu

---
+++ DeathIsTemporaryJam/Assets/Scripts/Models/GameState.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum GameState 
+{
+    PLAYING,
+    MAINMENU,
+    UPGRADE,
+    PAUSE,
+    GAMEOVER,
+}
